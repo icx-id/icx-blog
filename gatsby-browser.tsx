2f6fbe07ebb5f01ea9fcf0dfc95e77d6ecfca85b
theodevoid <87703322+theodevoid@users.noreply.github.com>
2023-05-03 10:36:19 +0700

    fix: optimized fonts and page container

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { GatsbyBrowser } from 'gatsby';
 import { RootProvider } from '~/providers';
 import { PageContainer } from '~/components';
+import '@fontsource/inter';
+
+const noNavbarFooterPaths = ['/login', '/register'];
+
+export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = ({ element, props }) => {
+  const { pathname } = props.location;
+  return noNavbarFooterPaths.includes(pathname) ? (
+    element
+  ) : (
+    <PageContainer>{element}</PageContainer>
+  );
+};
 
 export const wrapRootElement: GatsbyBrowser['wrapRootElement'] = ({ element }) => {
-  return (
-    <RootProvider>
-      <PageContainer>{element}</PageContainer>
-    </RootProvider>
-  );
+  return <RootProvider>{element}</RootProvider>;
 };
--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,6 +2,7 @@ import React, { PropsWithChildren } from 'react';
 import { ApiClientProvider } from '~/providers/ApiClientProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AxiosManager } from '~/lib/axios';
+import { MantineProvider } from '@mantine/core';
 
 const queryClient = new QueryClient();
 const axiosManager = new AxiosManager();
@@ -9,7 +10,29 @@ const axiosManager = new AxiosManager();
 export const RootProvider: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <ApiClientProvider axiosManager={axiosManager}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            fontFamily: 'Inter, Open Sans, sans serif',
+            components: {
+              Container: {
+                defaultProps: {
+                  sizes: {
+                    xs: 720,
+                    sm: 960,
+                    md: 1140,
+                    lg: 1320,
+                    xl: 1920,
+                  },
+                },
+              },
+            },
+          }}>
+          {children}
+        </MantineProvider>
+      </QueryClientProvider>
     </ApiClientProvider>
   );
 };
