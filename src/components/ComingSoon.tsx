aaf58f4be6db74fadc3e88987bf95334b8fe8007
hasbifadillah <hasbifadillah@outlook.com>
2023-05-04 11:02:18 +0700

    fix: handle force redirect to 404

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -5,7 +5,7 @@ export const ComingSoon: React.FC = () => {
   return (
     <div style={{ backgroundColor: 'black', height: '100vh' }}>
       <Container size="md">
-        <Center maw={400} h="calc(100vh - 15vh)" mx="auto">
+        <Center maw={400} h="85vh" mx="auto">
           <Image src="/img/logo-white.png" alt="ICX" maw={100} m="xl" />
           <Text color="white" fz="lg" fw="bold" lineClamp={2}>
             Unlocking Your Next Level Investment
