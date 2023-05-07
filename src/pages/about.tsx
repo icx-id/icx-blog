f245af3c1a37dcef93d47d5b53b55732473a1e35
kuntiarso <gilang.grk17@gmail.com>
2023-05-07 10:26:37 +0700

    fix: about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,17 +1,47 @@
 import React from 'react';
 import type { HeadFC, PageProps } from 'gatsby';
-import { NewsSection, OurStorySection, SummarySection } from '~/features/about';
+import { OurStorySection, SummarySection } from '~/features/about';
 import { SiteMetadata } from '~/components';
-import { Box } from '@mantine/core';
+import { Box, MediaQuery, Modal } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+const videoLink = 'https://www.youtube.com/embed/tgbNymZ7vqY';
+const videoTitle = 'Bohemian Rhapsody | Muppet Music Video | The Muppets';
 
 const AboutPage: React.FC<PageProps> = () => {
+  const [opened, { open: onOpenVideo, close: onCloseVideo }] = useDisclosure(true);
+
   return (
-    <Box sx={{ overflow: 'hidden' }}>
-      <SummarySection />
-      <OurStorySection />
-      {/* TODO: enable when ready */}
-      {/* <NewsSection /> */}
-    </Box>
+    <>
+      <Box>
+        <SummarySection onOpenVideo={onOpenVideo} />
+        <OurStorySection />
+        {/* TODO: enable when ready */}
+        {/* <NewsSection /> */}
+      </Box>
+
+      <Modal opened={opened} onClose={onCloseVideo} size="auto" centered>
+        <MediaQuery query="(max-width: 450px)" styles={{ width: '100%', height: 203 }}>
+          <MediaQuery query="(max-width: 570px)" styles={{ width: 360, height: 203 }}>
+            <MediaQuery query="(max-width: 850px)" styles={{ width: 480, height: 270 }}>
+              <MediaQuery query="(max-width: 1250px)" styles={{ width: 720, height: 405 }}>
+                <Box w={1080} h={608}>
+                  <iframe
+                    src={`${videoLink}?autoplay=1`}
+                    title={videoTitle}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    height="100%"
+                    width="100%"
+                  />
+                </Box>
+              </MediaQuery>
+            </MediaQuery>
+          </MediaQuery>
+        </MediaQuery>
+      </Modal>
+    </>
   );
 };
 
