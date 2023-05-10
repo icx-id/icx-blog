e87196c2bb814a4fcd6a3c6d217138fda72017c4
Katherine Davenia <katherine@landx.id>
2023-05-10 16:14:36 +0700

    font size

--- src/features/icxConnect/components/EventList.tsx
+++ src/features/icxConnect/components/EventList.tsx
@@ -6,21 +6,30 @@ import { parseEventDate } from '../utils';
 import { EventListSkeleton } from './EventListSkeleton';
 import { EventScheduleType } from '../types';
 import { navigate } from 'gatsby';
+import { useMediaQuery } from '@mantine/hooks';
 
 function capitalizeFirstLetter(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1) || '';
 }
+
 export const EventList: React.FC<{}> = () => {
   const { data: events, isLoading } = useGetEventsQuery({
     type: EventScheduleType.UPCOMING,
   });
+
+  const mobileScreen = useMediaQuery('(max-width: 30em)');
+  const tabScreen = useMediaQuery('(min-width: 30em) and (max-width: 60em)');
 
   return (
     <Box sx={{ minHeight: '100vh' }} pb="lg">
       <Paper
         sx={{
           backgroundImage: 'url(/img/events-banner.png)',
-          backgroundPosition: '-500px 0px',
+          backgroundPosition: mobileScreen
+            ? '-1200px 0px'
+            : tabScreen
+            ? '-1000px 0px'
+            : '-500px 0px',
           height: '20rem',
           borderRadius: '0px',
         }}>
@@ -33,10 +42,10 @@ export const EventList: React.FC<{}> = () => {
               height: '100%',
               paddingTop: '50px',
             }}>
-            <Text color="#FFF" fz={30} fw="bolder">
+            <Text color="#FFF" fz={mobileScreen ? 26 : 30} fw="bolder">
               ICX Connect
             </Text>
-            <Text color="#F1F3F5" fz={16} maw={420} fw="lighter">
+            <Text color="#F1F3F5" fz={mobileScreen ? 14 : 16} maw={420} fw="lighter">
               Jadi bagian dari komunitas angel investor, VCs, dan founder startup. Dapatkan akses ke
               event ekslusif.
             </Text>
