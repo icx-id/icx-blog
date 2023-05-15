9297d6ee3e97a5175766d496eeea56d0afe0fe3c
Theodore Rivaldo Devo Mangowal <87703322+theodevoid@users.noreply.github.com>
2023-05-15 14:09:38 +0700

    fix: responsive styles

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Image, Stack, Text } from '@mantine/core';
+import { Center, Container, Image, MediaQuery, Stack, Text } from '@mantine/core';
 import { HeadFC } from 'gatsby';
 import React from 'react';
 import { Link, SiteMetadata } from '~/components';
@@ -6,25 +6,35 @@ import logo404 from '~/images/404.svg';
 
 const NotFoundPage: React.FC = () => {
   return (
-    <Center h="100vh">
-      <Stack ta="center" align="center">
-        <Stack w={232} h={232}>
-          <Image src={logo404} alt="ICX" />
+    <Container>
+      <Center mih="100vh">
+        <Stack ta="center" align="center">
+          <MediaQuery smallerThan="md" styles={{ width: 150, height: 150 }}>
+            <Stack w={232} h={232}>
+              <Image src={logo404} alt="ICX" />
+            </Stack>
+          </MediaQuery>
+
+          <MediaQuery smallerThan="md" styles={{ fontSize: 24, marginTop: 0 }}>
+            <Text mt="lg" fz={48} fw="bold" c="#838383">
+              Something&apos;s missing
+            </Text>
+          </MediaQuery>
+
+          <MediaQuery smallerThan="md" styles={{ fontSize: 16, marginTop: 0 }}>
+            <Text mt="md" fz={20}>
+              We can&apos;t find the page you&apos;re looking for,
+              <br /> It seems like the page you&apos;re trying to reach has been moved,
+              <br /> deleted or simply doesn&apos;t exist.
+            </Text>
+          </MediaQuery>
+          <Link to="/">Back to Homepage</Link>
         </Stack>
-        <Text mt="lg" fz={48} fw="bold" c="#838383">
-          Something&apos;s missing
-        </Text>
-        <Text mt="mdF" fz={20}>
-          We can&apos;t find the page you&apos;re looking for,
-          <br /> It seems like the page you&apos;re trying to reach has been moved,
-          <br /> deleted or simply doesn&apos;t exist.
-        </Text>
-        <Link to="/">Back to Homepage</Link>
-      </Stack>
-    </Center>
+      </Center>
+    </Container>
   );
 };
 
-export default NotFoundPage;
-
 export const Head: HeadFC = () => <SiteMetadata />;
+
+export default NotFoundPage;
