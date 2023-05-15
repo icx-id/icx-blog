c9f565b0e17e3121c53734503452853c82f0cf35
Theodore Rivaldo Devo Mangowal <87703322+theodevoid@users.noreply.github.com>
2023-05-15 13:06:21 +0700

    feat: head

--- src/pages/icx-connect/index.tsx
+++ src/pages/icx-connect/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { PageProps } from 'gatsby';
+import { HeadFC, PageProps } from 'gatsby';
 import { EventList } from '~/features/icxConnect';
+import { SiteMetadata } from '~/components';
 
 const ICXConnectPage: React.FC<PageProps> = () => {
   return <EventList />;
 };
+
+export const Head: HeadFC = () => <SiteMetadata title="ICX Connect" />;
 
 export default ICXConnectPage;
