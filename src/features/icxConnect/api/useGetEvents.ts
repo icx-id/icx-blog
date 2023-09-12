4c03aa47d287d01d099f4eae2c2ef593410727a2
Katherine Davenia <katherine@landx.id>
2023-09-12 15:15:24 +0700

    wib

--- src/features/icxConnect/api/useGetEvents.ts
+++ src/features/icxConnect/api/useGetEvents.ts
@@ -2,9 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 import { useApiClient } from '~/providers/ApiClientProvider';
 import { PageableResponse, PaginationQueries } from '~/types/pagination';
 import { SimplifiedEvent, EventScheduleType } from '../types';
+import { EventAccess } from '~/features/event';
 
 interface EventQueries extends PaginationQueries {
   type?: EventScheduleType;
+  access?: EventAccess;
 }
 
 export const useGetEventsQuery = (queries?: EventQueries) => {
