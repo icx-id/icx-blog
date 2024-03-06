88d4affcd9a20c3bce84f3c86dd16d48aef4c18b
Hasbi Fadilah <hasbifadillah@outlook.com>
2024-03-06 10:44:55 +0700

    Release 6 Mar 2024 (#93)
    Co-authored-by: Stanley Purwahariyanto <stanleypurwaharianto@yahoo.com>
    Co-authored-by: Katherine Davenia <86787578+katherinedavenia@users.noreply.github.com>

--- src/components/Navbar/static/menus.ts
+++ src/components/Navbar/static/menus.ts
@@ -5,24 +5,24 @@ export type NavbarMenu = {
 };
 
 export const navbarMenus: NavbarMenu[] = [
-  {
-    id: 2,
-    name: 'Fundraise',
-    pathname: '/fundraise',
-  },
-  {
-    id: 3,
-    name: 'About Us',
-    pathname: '/about',
-  },
-  {
-    id: 4,
-    name: 'ICX Connect',
-    pathname: '/icx-connect',
-  },
-  {
-    id: 5,
-    name: 'Blog',
-    pathname: '/blog',
-  },
+  // {
+  //   id: 2,
+  //   name: 'Fundraise',
+  //   pathname: '/fundraise',
+  // },
+  // {
+  //   id: 3,
+  //   name: 'About Us',
+  //   pathname: '/about',
+  // },
+  // {
+  //   id: 4,
+  //   name: 'ICX Connect',
+  //   pathname: '/icx-connect',
+  // },
+  // {
+  //   id: 5,
+  //   name: 'Blog',
+  //   pathname: '/blog',
+  // },
 ];
